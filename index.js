1b69698b2bc8d4575c91d899f9b4f98e9c4b7492
Dima111000 <dimafuga2003@gmail.com>
2022-09-29 19:48:32 +0300

    new add

--- index.js
+++ index.js
@@ -17,4 +17,14 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World'))
 
-app.listen(process.env.PORT || port, () => console.log(`Сервер работает на порту ${port}`))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(process.env.PORT || port, () => console.log(`Сервер работает на порту ${port}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
